1babc55a62abc3da7f7a3a2119c26ac78471a3b5
jorgeduje <jorgeduje4@gmail.com>
2023-10-17 13:09:14 -0300

    se implementa flatlist para los pokemones en el home

---
+++ src/components/common/PokemonCard.jsx
@@ -0,0 +1,68 @@
+import { View, Text, TouchableOpacity, Image, StyleSheet } from "react-native";
+import React from "react";
+
+export default function PokemonCard({ pokemon }) {
+  return (
+    <TouchableOpacity onPress={() => console.log("seleccione un pokemon")}>
+      <View style={{ ...styles.cardContainer, backgroundColor: pokemon.color }}>
+        <Text style={styles.name}>
+          {pokemon.name} {pokemon.id}
+        </Text>
+        <View style={styles.pokeballContainer}>
+          <Image
+            source={require("../../assets/pokebola-blanca.png")}
+            style={styles.pokeball}
+          />
+        </View>
+        <Image source={{ uri: pokemon.picture }} style={styles.pokemonImage} />
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    marginHorizontal: 10,
+    height: 120,
+    width: 160,
+    marginBottom: 25,
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  name: {
+    color: "whitesmoke",
+    fontSize: 20,
+    top: 20,
+    left: 10,
+  },
+  pokeballContainer: {
+    width: 100,
+    height: 100,
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    overflow: "hidden",
+    opacity: 0.4,
+  },
+  pokeball: {
+    width: 100,
+    height: 100,
+    position: "absolute",
+    bottom: -25,
+    right: -25,
+  },
+  pokemonImage: {
+    width: 100,
+    height: 100,
+    position: "absolute",
+    right: -15,
+    bottom: -10,
+  },
+});
--- src/components/screens/HomeScreen.jsx
+++ src/components/screens/HomeScreen.jsx
@@ -1,10 +1,61 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { usePokemonPaginate } from "../../hooks/usePokemonPaginate";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import PokemonCard from "../common/PokemonCard";
 
 export default function HomeScreen() {
+  const { pokemonList, loadPokemons } = usePokemonPaginate();
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <>
+      <Image
+        source={require("../../assets/pokebola.png")}
+        style={styles.bgPokeball}
+      />
+      {pokemonList.length > 0 && (
+        <View style={styles.containerList}>
+          <FlatList
+            data={pokemonList}
+            numColumns={2}
+            keyExtractor={(pokemon) => String(pokemon.id)}
+            renderItem={({ item }) => <PokemonCard pokemon={item} />}
+            ListHeaderComponent={<Text style={styles.title}>Pokedex</Text>}
+            showsVerticalScrollIndicator={false}
+            ListFooterComponent={
+              <ActivityIndicator style={{ height: 200 }} size={50} />
+            }
+            onEndReached={loadPokemons}
+            onEndReachedThreshold={0.3}
+          />
+        </View>
+      )}
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  bgPokeball: {
+    width: 300,
+    height: 300,
+    position: "absolute",
+    top: -130,
+    right: -130,
+    marginHorizontal: 20,
+    opacity: 0.2,
+  },
+  containerList: {
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: "bold",
+    marginHorizontal: 20,
+  },
+});
